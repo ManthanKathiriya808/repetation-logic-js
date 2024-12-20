9da7f71acdda4d7ab825a31cc0120869d004354a
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-20 15:29:47 +0530

    defg

---
+++ repetation loops/index.js
@@ -0,0 +1,15 @@
+let a =1;
+
+for(let i =0 ; i<10;i++){
+console.log(a+i)
+}
+
+let obj ={
+    name: "manthan",
+    rolw:"prograsmmer",
+    company:"none"
+}
+
+for(const key in obj){
+    console.log(key)
+}
